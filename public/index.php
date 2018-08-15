4aa454c418afc3745d935e9cb855f4fc64ccb432
Danilo Dughetti <danilo.dughetti@gmail.com>
2018-08-15 14:59:51 +0200

    Aggiunto google Analytics

--- public/index.php
+++ public/index.php
@@ -99,5 +99,12 @@
             </div>
           </div>
         </div>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-68566370-4"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-68566370-4');
+            </script>
   </body>
 </html>
